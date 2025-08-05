7a7d469e9ceae5b01d8f7454571db0105a342fce
SzemjuskyPC <ozeiasfilho68@gmail.com>
2025-08-05 15:11:18 +0100

    teste

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,8 @@ import { CountDown } from "./components/CountDown";
 
 import "./assets/styles/theme.css";
 import "./assets/styles/global.css";
+import { DefaultInput } from "./components/DefaultInput";
+import { Cycles } from "./components/Cycles";
 
 export function App() {
   return (
@@ -24,14 +26,18 @@ export function App() {
       <Container>
         <form className="form" action="">
           <div className="formRow">
-            <label htmlFor="meuInput">task</label>
-            <input id="meuInput" type="text" />
+            <DefaultInput
+              labelText="alguma coisa"
+              id="meuInput"
+              type="text"
+              placeholder="Escreva ze"
+            />
           </div>
           <div>
             <p>Lorem ipsum dolor sit amet</p>
           </div>
           <div className="formRow">
-            <p>Ciclos</p>
+            <Cycles />
             <p> 0 0 0 0 0 0</p>
           </div>
           <div className="formRow">
