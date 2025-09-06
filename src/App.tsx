8741c2cec999b0bd4f3f6b8f3901775274a8c566
SzemjuskyPC <ozeiasfilho68@gmail.com>
2025-09-06 09:51:11 +0100

    indio trabalhar

--- src/App.tsx
+++ src/App.tsx
@@ -1,50 +1,9 @@
-import { Container } from "./components/Container";
-import { Menu } from "./components/Menu";
-import { Logo } from "./components/Logo";
-import { CountDown } from "./components/CountDown";
+import { Home } from "./pages/Home";
 
-import "./assets/styles/theme.css";
-import "./assets/styles/global.css";
-import { DefaultInput } from "./components/DefaultInput";
-import { Cycles } from "./components/Cycles";
+import "./styles/theme.css";
+
+import "./styles/global.css";
 
 export function App() {
-  return (
-    <>
-      <Container>
-        <Logo />
-      </Container>
-
-      <Container>
-        <Menu />
-      </Container>
-
-      <Container>
-        <CountDown />
-      </Container>
-
-      <Container>
-        <form className="form" action="">
-          <div className="formRow">
-            <DefaultInput
-              labelText="alguma coisa"
-              id="meuInput"
-              type="text"
-              placeholder="Escreva ze"
-            />
-          </div>
-          <div>
-            <p>Lorem ipsum dolor sit amet</p>
-          </div>
-          <div className="formRow">
-            <Cycles />
-            <p> 0 0 0 0 0 0</p>
-          </div>
-          <div className="formRow">
-            <button>Enviar</button>
-          </div>
-        </form>
-      </Container>
-    </>
-  );
+  return <Home />;
 }
--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,23 +1,83 @@
-import { HistoryIcon, HouseIcon, SunIcon, SettingsIcon } from "lucide-react";
+import {
+  HistoryIcon,
+  HouseIcon,
+  SunIcon,
+  SettingsIcon,
+  MoonIcon,
+} from "lucide-react";
 import styles from "./styles.module.css";
+import { useEffect, useState } from "react";
+
+type AvailableThemes = "dark" | "light";
 
 export function Menu() {
+  const [theme, setTheme] = useState<AvailableThemes>(() => {
+    const storageTheme =
+      (localStorage.getItem("theme") as AvailableThemes) || "dark";
+    return storageTheme;
+  });
+
+  const nextThemeIcon = {
+    dark: <SunIcon />,
+    light: <MoonIcon />,
+  };
+
+  function handleThemeChange(
+    event: React.MouseEvent<HTMLAnchorElement, MouseEvent>
+  ) {
+    event.preventDefault(); // Não segue o link
+
+    console.log("Clicado", Date.now());
+
+    setTheme((prevTheme) => {
+      const nextTheme = prevTheme === "dark" ? "light" : "dark";
+      return nextTheme;
+    });
+  }
+
+  useEffect(() => {
+    document.documentElement.setAttribute("data-theme", theme);
+    localStorage.setItem("theme", theme);
+  }, [theme]);
+
   return (
     <nav className={styles.menu}>
-      <a className={styles.menuLink} href="#">
+      <h1>{theme}</h1>
+      <a
+        className={styles.menuLink}
+        href="#"
+        aria-label="Ir para pagina inicial"
+        title="Ir para pagina inicial"
+      >
         <HouseIcon />
       </a>
 
-      <a className={styles.menuLink} href="#">
+      <a
+        className={styles.menuLink}
+        href="#"
+        aria-label="Ir para o historico"
+        title="Ir para o historico "
+      >
         <HistoryIcon />
       </a>
 
-      <a className={styles.menuLink} href="#">
+      <a
+        className={styles.menuLink}
+        href="#"
+        aria-label="Ir para configurações"
+        title="Ir para configurações"
+      >
         <SettingsIcon />
       </a>
 
-      <a className={styles.menuLink} href="#">
-        <SunIcon />
+      <a
+        className={styles.menuLink}
+        href="#"
+        aria-label="mudar tema"
+        title="Mudar tema"
+        onClick={handleThemeChange}
+      >
+        {nextThemeIcon[theme]}
       </a>
     </nav>
   );
