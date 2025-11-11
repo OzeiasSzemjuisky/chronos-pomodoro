6def9267fc58f5f3647435fd58f1137c197b3bb0
SzemjuskyPC <ozeiasfilho68@gmail.com>
2025-11-11 01:46:32 +0000

    fazendo a logica

--- src/components/MainForm/index.tsx
+++ src/components/MainForm/index.tsx
@@ -5,11 +5,17 @@ import { DefaultInput } from "../DefaultInput";
 import { useRef } from "react";
 import type { TaskModel } from "../../models/TaskModel";
 import { useTaskContext } from "../../contexts/TaskContext/useTaskContext";
+import { getNextCycle } from "../../utils/getNextCycle";
+import { getNextCycleType } from "../../utils/getNextCycleType";
+import { formatSecondsToMinutes } from "../../utils/formatSecondsToMinutes";
 
 export function MainForm() {
-  const { setState } = useTaskContext();
+  const { state, setState } = useTaskContext();
   // const [taskName, setTaskName] = useState(""); para quando tiver que ser em tempo real
   const taskNameInput = useRef<HTMLInputElement>(null); // sem atualizacao em tempo real
+
+  const nextCycle = getNextCycle(state.currentCycle); // Meio que a atualizacao esta sendo feita entre as renderizacoes , porque pega o ciclo atual, mas o proximo so vem com  o envio do form
+  const nextCycleType = getNextCycleType(nextCycle);
 
   function handleCreateNewTask(event: React.FormEvent<HTMLFormElement>) {
     event.preventDefault();
@@ -28,8 +34,8 @@ export function MainForm() {
       startDate: Date.now(),
       completeDate: null,
       interruptDate: null,
-      duration: 1,
-      type: "workTime",
+      duration: state.config[nextCycleType],
+      type: nextCycleType,
     };
 
     const secondsRemaining = newTask.duration * 60;
@@ -39,9 +45,9 @@ export function MainForm() {
         ...prevState,
         config: { ...prevState.config },
         activeTask: newTask,
-        currentCycle: 1, //conferir depois
+        currentCycle: nextCycle,
         secondsRemaining, //como as chaves sao iguais, o valor ja esta inbutido
-        formattedSecondsRemaining: "00:00",
+        formattedSecondsRemaining: formatSecondsToMinutes(secondsRemaining),
         task: [...prevState.tasks, newTask], //pegando o array passado porquenao se deve alterar o valor diretamente
       };
     });
